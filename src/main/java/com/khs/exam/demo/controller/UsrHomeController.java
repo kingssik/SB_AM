5b98ca846ea4cdaa59a293b9b9337ff45dc82e3b
kingssik <hyunsikkang25@gmail.com>
2022-09-26 00:51:10 +0900

    ArticleController 게시물 추가 구현, 리스트 구현 예정

--- src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
@@ -9,12 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.khs.exam.demo.vo.Article;
 
 @Controller
-public class UserHomeController {
+public class UsrHomeController {
 	@RequestMapping("usr/home/getString")
 	@ResponseBody
 	public String getString() {
@@ -74,31 +72,22 @@ public class UserHomeController {
 	@RequestMapping("usr/home/getArticle")
 	@ResponseBody
 	public Article getArticle() {
-		Article article = new Article(1, "제목1");
-		Article article2 = new Article();
+		Article article = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
 
 		return article;
 	}
-	
+
 	@RequestMapping("usr/home/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1");
-		Article article2 = new Article(2, "제목2");
-		
+		Article article1 = new Article(1, "제목1", "내용1");
+		Article article2 = new Article(2, "제목2", "내용2");
+
 		List<Article> articles = new ArrayList<>();
 		articles.add(article1);
 		articles.add(article2);
 
 		return articles;
 	}
-}
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class Article {
-	private int id;
-	private String title;
-
 }
