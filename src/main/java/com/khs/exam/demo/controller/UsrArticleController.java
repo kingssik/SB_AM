705d43720b2a356036fa2250263b0424cc13aaeb
kingssik <hyunsikkang25@gmail.com>
2022-09-26 23:59:14 +0900

    게시물 수정기능

--- src/main/java/com/khs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrArticleController.java
@@ -11,10 +11,11 @@ import com.khs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-
+	// 인스턴스 변수
 	private int lastArticleId;
 	private List<Article> articles;
 
+	// 생성자
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
@@ -22,6 +23,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 
+	// 서비스메서드
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목 " + i;
@@ -37,18 +39,12 @@ public class UsrArticleController {
 				return article;
 			}
 		}
-		return null;
-	}
 
-	private void deleteArticle(int id) {
-		Article article = getArticle(id);
-		
-		articles.remove(article);
+		return null;
 	}
 
 	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
-
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
@@ -57,6 +53,20 @@ public class UsrArticleController {
 		return article;
 	}
 
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+
+		articles.remove(article);
+	}
+
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
+
+	// 액션메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -79,8 +89,24 @@ public class UsrArticleController {
 		if (article == null) {
 			return id + "번 게시물은 존재하지 않습니다";
 		}
+
 		deleteArticle(id);
-		return id + "번 게시물이 삭제되었습니다";
+
+		return id + "번 게시물을 삭제했습니다";
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + "번 게시물을 수정했습니다";
 	}
 
 }
