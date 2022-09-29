70b6fd260c001ea4ae42b3a5da8c8a1837bd29dc
kingssik <hyunsikkang25@gmail.com>
2022-09-29 21:18:26 +0900

    회원가입시 입력된 데이터의 유효성 체크

--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -12,19 +12,43 @@ import com.khs.exam.demo.vo.Member;
 public class UsrMemberController {
 	@Autowired
 	private MemberService memberService;
-	
+
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		
-		if(id == -1) {
-			return "이미 존재하는 아이디입니다";
+
+		if (loginId == null || loginId.trim().length() == 0) {
+			return "아이디를 입력하세요";
 		}
 		
-		Member member = memberService.getMemberById(id);
+		if (loginPw == null || loginPw.trim().length() == 0) {
+			return "비밀번호를 입력하세요";
+		}
 		
+		if (name == null || name.trim().length() == 0) {
+			return "이름 입력하세요";
+		}
+		
+		if (nickname == null || nickname.trim().length() == 0) {
+			return "닉네임을 입력하세요";
+		}
+		
+		if (cellphoneNum == null || cellphoneNum.trim().length() == 0) {
+			return "전화번호를 입력하세요";
+		}
+		
+		if (email == null || email.trim().length() == 0) {
+			return "이메일을 입력하세요";
+		}
+
+		if (id == -1) {
+			return "이미 존재하는 아이디입니다";
+		}
+
+		Member member = memberService.getMemberById(id);
+
 		return member;
 	}
 
