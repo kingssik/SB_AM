a1bab8f1cadc1d94cbd6a5234e2cc687fa7e9b6a
kingssik <hyunsikkang25@gmail.com>
2022-09-29 23:00:07 +0900

    Ut.f 함수 도입, 편해진 문장 구성

--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -23,33 +23,33 @@ public class UsrMemberController {
 		if (Ut.empty(loginId)) {
 			return "아이디를 입력하세요";
 		}
-		
+
 		if (Ut.empty(loginPw)) {
 			return "비밀번호를 입력하세요";
 		}
-		
+
 		if (Ut.empty(name)) {
 			return "이름을 입력하세요";
 		}
-		
+
 		if (Ut.empty(nickname)) {
 			return "닉네임을 입력하세요";
 		}
-		
+
 		if (Ut.empty(cellphoneNum)) {
 			return "전화번호를 입력하세요";
 		}
-		
+
 		if (Ut.empty(email)) {
 			return "이메일을 입력하세요";
 		}
 
 		if (id == -1) {
-			return "이미 존재하는 아이디입니다";
+			return Ut.f("이미 존재하는 아이디(%s)입니다", loginId);
 		}
-		
+
 		if (id == -2) {
-			return "이미 사용중인 이름과 이메일입니다";
+			return Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email);
 		}
 
 		Member member = memberService.getMemberById(id);
