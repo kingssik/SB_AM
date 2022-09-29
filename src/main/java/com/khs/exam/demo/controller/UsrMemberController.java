90db6b089939bde4b068c130b1919f4a51b34992
kingssik <hyunsikkang25@gmail.com>
2022-09-29 21:02:46 +0900

    로그인 아이디 중복체크

--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -15,9 +15,13 @@ public class UsrMemberController {
 	
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+		
+		if(id == -1) {
+			return "이미 존재하는 아이디입니다";
+		}
 		
 		Member member = memberService.getMemberById(id);
 		
--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -8,14 +8,19 @@ import com.khs.exam.demo.vo.Member;
 @Service
 public class MemberService {
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
 
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		Member existsMember = memberRepository.getMemberByLoginId(loginId);
+
+		if (existsMember != null) {
+			return -1;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		
 		return memberRepository.getLastInsertId();
 
 	}
