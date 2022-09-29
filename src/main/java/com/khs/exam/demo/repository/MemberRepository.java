d39fbc65eb051df2c23f1440003682b4d27c5b77
kingssik <hyunsikkang25@gmail.com>
2022-09-29 20:45:16 +0900

    회원가입 완료 후 생성된 회원의 정보를 보여줌

--- src/main/java/com/khs/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/khs/exam/demo/repository/MemberRepository.java
@@ -2,21 +2,34 @@ package com.khs.exam.demo.repository;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.khs.exam.demo.vo.Member;
 
 @Mapper
 public interface MemberRepository {
-	
+
 	@Insert("""
-			INSERT INTO `member` 
+			INSERT INTO `member`
 			SET regDate = NOW(),
 			updateDate = NOW(),
 			loginId = #{loginId},
 			loginPw = #{loginPw},
-			`name` = #{name}, 
+			`name` = #{name},
 			nickname = #{nickname},
 			cellphoneNum = #{cellphoneNum},
 			email = #{email}
 					""")
 	void join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email);
+
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
+
+	@Select("""
+			SELECT *
+			FROM `member` AS M
+			WHERE M.id = #{id}
+			""")
+	Member getMemberById(int id);
 
 }
