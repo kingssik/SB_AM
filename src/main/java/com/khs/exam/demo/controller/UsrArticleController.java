b08063f2c02fd9f4a0d8dfe8be39bc92788085d7
kingssik <hyunsikkang25@gmail.com>
2022-10-04 22:02:51 +0900

    usr/article/doDelete, usr/article/doModify 에 ResultData 적용

--- src/main/java/com/khs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrArticleController.java
@@ -41,30 +41,30 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다";
+			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id), id);
 		}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 게시물을 삭제했습니다";
+		return ResultData.from("S-1",Ut.f("%d번 게시물이 삭제되었습니다.", id), id);
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Object doModify(int id, String title, String body) {
+	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다";
+			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.", id), id);
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return article;
+		return ResultData.from("S-1",Ut.f("%d번 게시물이 수정되었습니다.", id), id);
 	}
 
 	@RequestMapping("/usr/article/getArticle")
