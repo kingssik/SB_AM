1e3fcaf6704b20fc590f8a036c50b6e7109d8570
kingssik <hyunsikkang25@gmail.com>
2022-10-05 23:25:22 +0900

    JSP연결

--- src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	@RequestMapping("usr/home/main")
-	@ResponseBody
-	public String main() {
-		return "hi";
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "usr/home/main";
+	}
+	
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 
 }
