4ab39fb535fab58a2350e96d4ef77caa59e048b0
kingssik <hyunsikkang25@gmail.com>
2022-10-06 12:19:51 +0900

    게시물리스트 JSP로 구현

--- src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrHomeController.java
@@ -2,7 +2,6 @@ package com.khs.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
@@ -10,10 +9,9 @@ public class UsrHomeController {
 	public String showMain() {
 		return "usr/home/main";
 	}
-	
+
 	@RequestMapping("/")
 	public String showRoot() {
 		return "redirect:/usr/home/main";
 	}
-
 }
--- src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -7,6 +7,11 @@
 <title>메인</title>
 </head>
 <body>
-	<div>메인페이지</div>
+	<h1>메인페이지</h1>
+	<div>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+	Enim illum libero a ea omnis doloremque id dolorem earum nihil 
+	dicta tempora quibusdam dolorum ex quasi placeat veritatis error quisquam in.
+	</div>
 </body>
 </html>
