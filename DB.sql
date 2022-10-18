1d2819984bf06ec64993c817457a8841acbb4d19
kingssik <hyunsikkang25@gmail.com>
2022-10-18 21:15:34 +0900

    페이징 준비, 게시물 대량생성

--- DB.sql
+++ DB.sql
@@ -122,7 +122,7 @@ INSERT INTO board
 SET regDate = NOW(),
 updateDate = NOW(),
 `code` = 'free1',
-`name` = '자유게시판';
+`name` = '자유';
 
 # 게시물 테이블에 boardId 칼럼 추가
 ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
@@ -138,4 +138,13 @@ SET boardId = 2
 WHERE id IN (3);
 
 SELECT LAST_INSERT_ID();
+
+# 게시물 갯수 늘리기
+INSERT INTO article
+(
+    regDate, updateDate, memberId, boardId, title, `body`
+)
+SELECT NOW(), NOW(), FLOOR(RAND() * 2) + 1, FLOOR(RAND() * 2) + 1, CONCAT('제목_', RAND()), CONCAT('내용_', RAND())
+FROM article;
+
 
