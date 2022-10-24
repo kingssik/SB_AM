67afc56afeaf10ffbb0427fd9d3ea9f3c285a156
kingssik <hyunsikkang25@gmail.com>
2022-10-24 20:58:43 +0900

    reactionPoint 테이블 추가

--- src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/khs/exam/demo/controller/UsrMemberController.java
@@ -18,7 +18,7 @@ public class UsrMemberController {
 	private MemberService memberService;
 	@Autowired
 	private Rq rq;
-	
+
 	@RequestMapping("usr/member/join")
 	public String showJoin() {
 		return "usr/member/join";
@@ -26,35 +26,32 @@ public class UsrMemberController {
 
 	@RequestMapping("usr/member/doJoin")
 	@ResponseBody
-	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public String doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 
 		if (Ut.empty(loginId)) {
-			return ResultData.from("F-1", "아이디를 입력하세요");
+			return Ut.jsHistoryBack("아이디를 입력하세요");
 		}
 		if (Ut.empty(loginPw)) {
-			return ResultData.from("F-2", "비밀번호를 입력하세요");
+			return Ut.jsHistoryBack("비밀번호를 입력하세요");
 		}
 		if (Ut.empty(name)) {
-			return ResultData.from("F-3", "이름을 입력하세요");
+			return Ut.jsHistoryBack("이름을 입력하세요");
 		}
 		if (Ut.empty(nickname)) {
-			return ResultData.from("F-4", "닉네임을 입력하세요");
+			return Ut.jsHistoryBack("닉네임을 입력하세요");
 		}
 		if (Ut.empty(cellphoneNum)) {
-			return ResultData.from("F-5", "전화번호를 입력하세요");
+			return Ut.jsHistoryBack("전화번호를 입력하세요");
 		}
 		if (Ut.empty(email)) {
-			return ResultData.from("F-6", "이메일을 입력하세요");
+			return Ut.jsHistoryBack("이메일을 입력하세요");
 		}
-		// S-1
-		// 회원가입이 완료되었습니다
-		// F-1~8
-		// 실패
-		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
-		if (joinRd.isFail()) {
-			return (ResultData) joinRd;
+		String joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
+
+		if (joinRd.isEmpty()) {
+			return joinRd;
 		}
 
 		Member member = memberService.getMemberById(joinRd.getData1());
--- src/main/java/com/khs/exam/demo/service/MemberService.java
+++ src/main/java/com/khs/exam/demo/service/MemberService.java
@@ -15,26 +15,26 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public String join(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
 		// 로그인아이디 중복체크
 		Member existsMember = getMemberByLoginId(loginId);
 
 		if (existsMember != null) {
-			return ResultData.from("F-7", Ut.f("이미 사용중인 아이디(%s)입니다", loginId));
+			return Ut.jsHistoryBack(Ut.f("이미 사용중인 아이디(%s)입니다", loginId));
 		}
 
 		// 이름 + 이메일 중복체크
 		existsMember = getMemberByNameAndEmail(name, email);
 
 		if (existsMember != null) {
-			return ResultData.from("F-8", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email));
+			return Ut.jsHistoryBack(Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email));
 		}
 
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 		int id = memberRepository.getLastInsertId();
 
-		return ResultData.from("S-1", "회원가입이 완료되었습니다", "id", id);
+		return Ut.jsReplace("회원가입이 완료되었습니다", "../home/main");
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
