a2f5818da54ff62b4e1d08f8f63bddd49ab1e7b3
kingssik <hyunsikkang25@gmail.com>
2022-10-27 21:47:20 +0900

    버튼 누를 때 좋아요, 싫어요 구현 중

--- src/main/java/com/khs/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/khs/exam/demo/config/MyWebMvcConfigurer.java
@@ -2,6 +2,7 @@ package com.khs.exam.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,13 +20,24 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	NeedLoginInterceptor needLoginInterceptor;
 
 	// 인터셉터 적용
+	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
-				.excludePathPatterns("/error");
-		
-		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/doWrite")
-				.addPathPatterns("/usr/article/write").addPathPatterns("/usr/article/doDelete")
-				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/modify");
-	}
 
+		InterceptorRegistration ir;
+
+		ir = registry.addInterceptor(beforeActionInterceptor);
+		ir.addPathPatterns("/**");
+		ir.addPathPatterns("/favicon.ico");
+		ir.excludePathPatterns("/resource/**");
+		ir.excludePathPatterns("/error");
+
+		ir = registry.addInterceptor(needLoginInterceptor);
+		ir.addPathPatterns("/usr/article/write");
+		ir.addPathPatterns("/usr/article/doWrite");
+		ir.addPathPatterns("/usr/article/modify");
+		ir.addPathPatterns("/usr/article/doModify");
+		ir.addPathPatterns("/usr/article/doDelete");
+		ir.addPathPatterns("/usr/reactionPoint/doGoodReaction");
+		ir.addPathPatterns("/usr/reactionPoint/doBadReaction");
+	}
 }
